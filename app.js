4638112eb4d19450da1a20ea0f9e006e2240dfd4
mehmetali10 <mhmtali2316@gmail.com>
2022-11-17 01:12:17 +0300

    add post method

--- app.js
+++ app.js
@@ -6,21 +6,28 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-
+var items = ["Wake up", "have breakfast", "Go to work !"];
 
 app.get("/", (req,res)=>{
     var today = new Date();
     var currentDay = today.getDay();
-    var day = dayOfWeekAsInteger(currentDay);
 
-    res.render("list", {kindOfDay : day});
+    var options = {
+        weekday : "long",
+        day : "numeric",
+        month : "numeric"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay : day, collection: items});
 });
 
-function dayOfWeekAsInteger(day) {
-    var arr =  ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    return arr[day];    
-}
-
+app.post("/" , (req, res) => {
+    var item = req.body.td;
+    items.push(item);
+    res.redirect("/");
+});
 
 app.listen(3000, ()=>{
     console.log("server has started on port 3000.")
