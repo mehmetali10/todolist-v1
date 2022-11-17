c46d92f3341cdc5085e6ad599018bfa344a71296
mehmetali10 <mhmtali2316@gmail.com>
2022-11-18 01:46:52 +0300

    add styles, layout and export date

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-var items = ["Wake up", "have breakfast", "Go to work !"];
+
+const items = ["Wake up", "have breakfast", "Go to work !"];
+const workItems = ["add new feature", "refactor code", "make test"];
+
 
 app.get("/", (req,res)=>{
-    var today = new Date();
-    var currentDay = today.getDay();
-
-    var options = {
-        weekday : "long",
-        day : "numeric",
-        month : "numeric"
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", {kindOfDay : day, collection: items});
+    let day = date.getDay();
+    res.render("list", {listTitle : day, collection: items});
 });
+
 
 app.post("/" , (req, res) => {
-    var item = req.body.td;
-    items.push(item);
-    res.redirect("/");
+
+    let item = req.body.td;
+    console.log(req.body.list);
+
+    if(req.body.list =="Work")
+    {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else
+    {
+        items.push(item);
+        res.redirect("/");
+    }   
 });
+
+
+app.get("/work", (req, res)=>{
+    res.render("list", {listTitle: "Work List", collection: workItems});
+});
+
 
 app.listen(3000, ()=>{
     console.log("server has started on port 3000.")
 });
 
-
-/*
-
-<% 'Scriptlet' tag, for control-flow, no output
-<%_ ‘Whitespace Slurping’ Scriptlet tag, strips all whitespace before it
-<%= Outputs the value into the template (HTML escaped)
-<%- Outputs the unescaped value into the template
-<%# Comment tag, no execution, no output
-<%% Outputs a literal '<%'
-%> Plain ending tag
--%> Trim-mode ('newline slurp') tag, trims following newline
-_%> ‘Whitespace Slurping’ ending tag, removes all whitespace after it
-
-*/
